590e83feb83c1c33775afa61ea8ff28270dec83e
Chainn <578579786@qq.com>
2017-06-02 09:26:32 +0800

    Add list

---
+++ liaoxuefeng/basic/list.py
@@ -0,0 +1,10 @@
+classmates = ['Michael', 'Bob', 'Tracy']
+print len(classmates)
+classmates.append('xuchen')
+print classmates
+print classmates.pop()
+print classmates
+classmates.insert(1, 'Chainn')
+print classmates
+classmates.pop(1)
+print classmates
