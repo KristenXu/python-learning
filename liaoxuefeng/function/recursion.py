25dceceb5a2e7509f7a81eaebd73e80e13dc1c34
Chainn <578579786@qq.com>
2017-06-03 22:29:22 +0800

    Add recursion

---
+++ liaoxuefeng/function/recursion.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+def fact(n):
+    if n==1:
+        return 1
+    return n * fact(n - 1)
+
+#尾递归
+def fact2(n):
+    return fact_iter(n, 1)
+
+def fact_iter(num, product):
+    if num == 1:
+        return product
+    return fact_iter(num - 1, num * product)
+#遗憾的是，大多数编程语言没有针对尾递归做优化，Python解释器也没有做优化，所以，即使把上面的fact(n)函数改成尾递归方式，也会导致栈溢出。
