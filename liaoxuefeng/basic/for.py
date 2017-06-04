526e052943985c5eeb7c00818b9726c709ae4130
Chainn <578579786@qq.com>
2017-06-04 16:18:53 +0800

    Add index for for loop

--- liaoxuefeng/basic/for.py
+++ liaoxuefeng/basic/for.py
@@ -2,3 +2,9 @@ sum = 0
 for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
     sum = sum + x
 print sum
+sum2 = 0
+for index,x  in enumerate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]):
+    print index
+    sum2 = sum2 + x
+print sum2
+
