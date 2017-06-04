af4ed8181bff382bc335aa932f8cb39e7797a605
Chainn <578579786@qq.com>
2017-06-04 16:23:20 +0800

    Add iterate dictionary

--- liaoxuefeng/basic/for.py
+++ liaoxuefeng/basic/for.py
@@ -8,3 +8,11 @@ for index,x  in enumerate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]):
     sum2 = sum2 + x
 print sum2
 
+for x, y in [(1, 1), (2, 4), (3, 9)]:
+    print x, y
+
+d = {'a': 1, 'b': 2, 'c': 3}
+for key in d:
+    print key
+for k, v in d.iteritems():
+    print k, v
